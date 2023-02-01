b7d4976c5a4937d66f4d78cddbdc1d59243db5f6
Meron abera <88728877+Meron-Abera@users.noreply.github.com>
2023-02-01 05:32:00 -1000

    Create Grading students.py

---
+++ Grading students.py
@@ -0,0 +1,21 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+n = int(input().strip())
+for a0 in range(n):
+    grade = int(input().strip())
+    if grade >= 38:
+        mod5 = grade % 5
+        if mod5 >= 3:
+            grade = grade + (5 - mod5)
+    print(grade)
